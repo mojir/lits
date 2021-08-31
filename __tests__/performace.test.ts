4f696d39b3c6fa17e8d26638fc406e0414a040d9
Albert Mojir <albert.mojir@youcruit.com>
2021-08-31 20:27:41 +0200

    Added basic performance test for a simple expression

---
+++ __tests__/performace.test.ts
@@ -0,0 +1,75 @@
+/* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { executeProgram } from '../src'
+import { evaluateProgram } from '../src/evaluator'
+import { parseProgram } from '../src/parser'
+import { tokenize } from '../src/tokenizer'
+
+const ITERATIONS = 25000
+const program = `(+ (* (- x y) (- y x)) (* (/ x y) (/ y x)))`
+const context = { x: 20, y: 30 }
+const jsExpression = `((x - y) * (y - x)) + ((x / y) * (y / x))`
+
+// Some baseline values for javascript eval to compare with
+const startRefTime = Date.now()
+for (let i = 0; i < ITERATIONS; i += 1) {
+  const jsProgram = createJsProgram(jsExpression, context)
+  eval(jsProgram)
+}
+const averageRefTime = Math.round((100000 * (Date.now() - startRefTime)) / ITERATIONS) / 100
+
+function createJsProgram(expression: string, context: any) {
+  const vars = Object.entries(context)
+    .map(entry => `  var ${entry[0]} = ${JSON.stringify(entry[1])};`)
+    .join('\n')
+  return `
+(function () {
+${vars}
+return ${expression}
+})()
+`
+}
+
+function logPerformace(label: string, time: number) {
+  const averageTime = Math.round((100000 * time) / ITERATIONS) / 100
+  console.log(`
+${label}: ${averageTime} µs
+Factor: ${Math.round((100 * averageTime) / averageRefTime) / 100} (${averageRefTime} µs)`)
+}
+
+xdescribe('performace', () => {
+  test('tokenise', () => {
+    const startTime = Date.now()
+    for (let i = 0; i < ITERATIONS; i += 1) {
+      tokenize(program)
+    }
+    logPerformace('Tokenise', Date.now() - startTime)
+  })
+
+  test('parseProgram', () => {
+    const tokens = tokenize(program)
+    const startTime = Date.now()
+    for (let i = 0; i < ITERATIONS; i += 1) {
+      parseProgram(tokens)
+    }
+    logPerformace('Parse tokens', Date.now() - startTime)
+  })
+
+  test('evaluateProgram', () => {
+    const tokens = tokenize(program)
+    const ast = parseProgram(tokens)
+    const startTime = Date.now()
+    for (let i = 0; i < ITERATIONS; i += 1) {
+      evaluateProgram(ast, context)
+    }
+    logPerformace('Evaluate AST', Date.now() - startTime)
+  })
+
+  test('executeProgram tokenize - parse - evaluate', () => {
+    const startTime = Date.now()
+    for (let i = 0; i < ITERATIONS; i += 1) {
+      executeProgram(program, { x: 3, y: 4 })
+    }
+    logPerformace('Execute program (tokenize, parse and evaluate)', Date.now() - startTime)
+  })
+})
