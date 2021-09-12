aebaa5e6148e1a39634d3b50ff4db24ef8f023b4
Albert Mojir <albert.mojir@gmail.com>
2021-09-12 23:23:48 +0200

    Styled documentation page

---
+++ docs/scripts.js
@@ -0,0 +1,101 @@
+;(function () {
+  var id = location.hash.substring(1) || 'index'
+  showPage(id, 'replace')
+})()
+
+window.addEventListener('popstate', () => {
+  var id = location.hash.substring(1) || 'index'
+  showPage(id, 'none')
+})
+
+function play() {
+  var code = document.getElementById('lisp-textarea').value
+  var contextString = document.getElementById('context-textarea').value
+  var output = document.getElementById('output-textarea')
+  var context
+  try {
+    context = JSON.parse(contextString)
+  } catch (e) {
+    output.innerHTML = 'Error: Could not parse context'
+    output.classList.add('error')
+    return
+  }
+  var result
+  var oldLog = console.log
+  console.log = function () {
+    var args = Array.from(arguments)
+    var logRow = args.map(arg => '' + arg).join(' ')
+    var textarea = document.getElementById('log-textarea')
+    var oldInnerHTML = textarea.innerHTML
+    var newContent = oldInnerHTML ? oldInnerHTML + '\n' + logRow : logRow
+    textarea.innerHTML = newContent
+    textarea.scrollTop = textarea.scrollHeight
+    if (newContent) {
+      document.getElementById('clear-log').classList.add('active')
+    }
+  }
+  try {
+    result = lispish.lispish(code, context)
+  } catch (error) {
+    output.innerHTML = error
+    output.classList.add('error')
+    return
+  } finally {
+    console.log = oldLog
+  }
+  output.classList.remove('error')
+  var content = stringifyValue(result)
+  output.innerHTML = content
+  if (content) {
+    document.getElementById('clear-output').classList.add('active')
+  }
+}
+function showPage(id, historyEvent) {
+  var els = document.getElementsByClassName('active-content')
+  while (els[0]) {
+    els[0].classList.remove('active-content')
+  }
+  els = document.getElementsByClassName('active-sidebar-entry')
+  while (els[0]) {
+    els[0].classList.remove('active-sidebar-entry')
+  }
+
+  if (id === 'index') {
+    document.getElementById('home-link').classList.remove('active')
+    document.getElementById('index').classList.add('active-content')
+  } else {
+    var docPage = document.getElementById(id)
+    if (docPage) {
+      docPage.classList.add('active-content')
+      document.getElementById(id + '_link').classList.add('active-sidebar-entry')
+    } else {
+      showPage('index', 'replace')
+      return
+    }
+    document.getElementById('home-link').classList.add('active')
+  }
+  if (historyEvent === 'none') {
+    return
+  } else if (historyEvent === 'replace') {
+    history.replaceState(null, '', '#' + id)
+  } else {
+    history.pushState(null, '', '#' + id)
+  }
+}
+
+function stringifyValue(value) {
+  return JSON.stringify(value, (k, v) => (v === undefined ? 'b234ca78-ccc4-5749-9384-1d3415d29423' : v)).replace(
+    /"b234ca78-ccc4-5749-9384-1d3415d29423"/g,
+    'undefined',
+  )
+}
+
+function clearOutput() {
+  document.getElementById('output-textarea').innerHTML = ''
+  document.getElementById('clear-output').classList.remove('active')
+}
+
+function clearLog() {
+  document.getElementById('log-textarea').innerHTML = ''
+  document.getElementById('clear-log').classList.remove('active')
+}
---
+++ scripts/docs/buildDocs.js
@@ -0,0 +1,208 @@
+const { functionReference, categories } = require('../../cli/reference')
+const lispish = require('../../dist/lispish')
+const path = require('path')
+const fs = require('fs')
+
+const DOC_DIR = path.resolve(__dirname, '../../docs')
+
+setupDocDir()
+copyScripts()
+copyStyles()
+writeIndexPage()
+
+function writeIndexPage() {
+  const page = `<!DOCTYPE html>
+<html>
+${getHeader()}
+<body>
+  ${getTopBar({ back: false })}
+  <div id="page" class="row">
+    ${getSideBar()}
+    <div id="main-panel">
+      ${getIndexContent()}
+      ${Object.values(functionReference)
+        .map(obj => getDocumentationContent(obj))
+        .join('\n')}
+    </div>
+  </div>
+  ${getPlayground()}
+  <script src="https://cdn.jsdelivr.net/npm/lodash@4.17.21/lodash.min.js" ></script>
+  <script src="lispish.iife.js"></script>
+  <script src='scripts.js'></script>
+</body>
+</html>
+`
+  fs.writeFileSync(path.join(DOC_DIR, `index.html`), page, { encoding: 'utf-8' })
+}
+
+function getTopBar() {
+  return `
+<div id="top-bar">
+  <div class="row">
+    <div class="column">
+      <a id="home-link" onclick="showPage('index')">Home</a>
+    </div>
+    <div class="column header">Lispish</div>
+    <div class="column">
+    </div>
+  </div>
+</div>`
+}
+
+function getHeader() {
+  return `
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="styles.css">
+</head>
+`
+}
+
+function getPlayground() {
+  return `
+<div id="playground">
+  <div class="header"><span class="icon-button" onclick="play()">▶ Playground</span></div>
+  <div class="row">
+    <div class="column" id="context">
+      <div class="textarea-header">Context (JSON)</div>
+      <textarea spellcheck=false rows="12" id="context-textarea">{ "x": 12 }</textarea>
+    </div>
+    <div class="column" id="lisp">
+      <div class="textarea-header">Lisp</div>
+      <textarea spellcheck=false rows="12" id="lisp-textarea">(setq y 5)\n\n(write "y" y)\n\n(write (* x y))</textarea>
+    </div>
+    <div class="column" id="output">
+      <div class="row">
+        <div class="column textarea-header">Result</div>
+        <div class="column small right"><span id="clear-output" class="icon-button" onclick="clearOutput()">✖</span></div>
+      </div>
+      <textarea id="output-textarea" readonly spellcheck=false rows="12" id="context"></textarea>
+    </div>
+    <div class="column" id="log">
+      <div class="row">
+        <div class="column textarea-header">Console log</div>
+        <div class="column small right"><span id="clear-log" class="icon-button" onclick="clearLog()">✖</span></div>
+      </div>
+      <textarea id="log-textarea" readonly spellcheck=false rows="12" id="context"></textarea>
+    </div>
+  </div>
+</div>
+`
+}
+
+function getIndexContent() {
+  return `
+<div id="index" class="content">
+  <h1>Welcome to Lispish!</h1>
+  <br />
+  <p>Lispish is a Lisp dialect made to work well in a browser or Node environment.</p>
+  <p>Quite a lot in Lispish is not what you're used to if you've done some Lisp before.</p>
+  <ul>
+    <li><pre>t</pre> and <pre>nil</pre> are gone. Instead there are four new symbols: <pre>true</pre>, <pre>false</pre>, <pre>null</pre> and <pre>undefined</pre>.</li>
+    <li>Only one sequence type exists: <pre>list</pre>. And its undelaying data structure is a javascript array.</li>
+    <li>No quotes! <pre>'(1 2 3)</pre> is no more... Use <pre>(list 1 2 3)</pre> instead.</li>
+    <li>No macros.</li>
+    <li>No keyword symbols e.g. <pre>:foo</pre>.</li>
+    <li>No tail call optimization (yet).</li>
+    <li>No dotted pairs.</li>
+    <li>100% test coverage</li>
+  </ul>
+  <p>Have a look at the list of functions to the left. These are what is available in terms of special- and normal expressions.</p>
+  <p>For more instruction on how to install and use Lispish as a cli or a typescript lib, checkout <a href="https://github.com/mojir/lispish">https://github.com/mojir/lispish</a></p>
+  <p/>
+  <p>Happy coding!</p>
+</div>
+`
+}
+
+function getDocumentationContent(docObj) {
+  const { name, longDescription, syntax, linkName, specialExpression, examples, sideEffects, arguments: args } = docObj
+  return `
+<div id="${linkName}" class="content function">
+  <div class="function-header">${name}</div>
+  ${specialExpression ? '<h3>Special Expression</h3>' : ''}
+  <p>${longDescription}</p>
+  <label>Syntax</label>
+  <pre>${syntax}</pre>
+  <label>Arguments</label>
+  <pre>${args.length === 0 ? 'No arguments' : args.map(arg => `${arg.name}: ${arg.type}`).join('<br />')}</pre>
+  <label>Side effects</label>
+  <pre>${sideEffects.length === 0 ? 'No side effects' : sideEffects.map(effect => effect).join('<br />')}</pre>
+  <label>Examples</label>
+  ${examples
+    .map(example => {
+      var oldLog = console.log
+      console.log = function () {}
+      var result
+      try {
+        result = lispish.lispish(example)
+        return `<pre>${example} => ${stringifyValue(result)}</pre>`
+      } catch (error) {
+        return `<pre>${example} => Error!</pre>`
+      } finally {
+        console.log = oldLog
+      }
+    })
+    .join('\n')}
+</div>
+`
+}
+
+function getSideBar() {
+  const categoryCollections = Object.values(functionReference).reduce((result, obj) => {
+    result[obj.category] = result[obj.category] || []
+    result[obj.category].push(obj)
+    return result
+  }, {})
+
+  return `
+<div id="sidebar">
+  ${categories
+    .map(categoryKey => {
+      return `
+        <label>${categoryKey}</label>
+        <ul>
+          ${
+            categoryCollections[categoryKey]
+              ? categoryCollections[categoryKey]
+                  .map(obj => {
+                    const linkName = obj.linkName
+                    const name = escape(obj.name)
+                    return `<a onclick="showPage('${linkName}')"><li id="${linkName}_link">${name}</li></a>`
+                  })
+                  .join('\n')
+              : ''
+          }
+        </ul>`
+    })
+    .join('\n')}
+</div>
+`
+}
+
+function setupDocDir() {
+  fs.rmdirSync(DOC_DIR, { recursive: true, force: true })
+  fs.mkdirSync(DOC_DIR)
+}
+
+function copyScripts() {
+  fs.copyFileSync(path.join(__dirname, '../../dist/lispish.iife.js'), path.join(DOC_DIR, 'lispish.iife.js'))
+  fs.copyFileSync(path.join(__dirname, `scripts.js`), path.join(DOC_DIR, `scripts.js`))
+}
+
+function copyStyles() {
+  fs.copyFileSync(path.join(__dirname, `styles.css`), path.join(DOC_DIR, `styles.css`))
+}
+
+function escape(str) {
+  str = str.replace(/>/g, '&gt;')
+  str = str.replace(/</g, '&lt;')
+  return str
+}
+
+function stringifyValue(value) {
+  return JSON.stringify(value, (k, v) => (v === undefined ? 'b234ca78-ccc4-5749-9384-1d3415d29423' : v)).replace(
+    /"b234ca78-ccc4-5749-9384-1d3415d29423"/g,
+    'undefined',
+  )
+}
