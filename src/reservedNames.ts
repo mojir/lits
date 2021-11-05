a1912590cea44899aca0f2cc8ccdae01213a859c
Robin Wallberg <robin.wallberg@youcruit.com>
2021-11-05 14:16:38 +0100

    Added reserved words.

--- src/reservedNames.ts
+++ src/reservedNames.ts
@@ -1,10 +1,12 @@
 import { Any } from './interface'
 
-export type ReservedName = `true` | `false` | `nil`
+export type ReservedName = `true` | `false` | `nil` | `null` | `undefined`
 export const reservedNamesRecord: Record<ReservedName, { value: Any }> = {
   true: { value: true },
   false: { value: false },
   nil: { value: null },
+  null: { value: null },
+  undefined: { value: null },
 }
 
 export const reservedNames: ReservedName[] = Object.keys(reservedNamesRecord) as ReservedName[]
