4592b4efef44be10c3dc3d1b9b3275eda4f0dbb6
Albert Mojir <albert.mojir@youcruit.com>
2021-11-08 09:12:35 +0100

    Added email regexp test

--- __tests__/builtin/normalExpressions/regexp.test.ts
+++ __tests__/builtin/normalExpressions/regexp.test.ts
@@ -26,6 +26,17 @@ describe(`regexp functions`, () => {
       expect(() => lits.run(`(regexp '' 'ab')`)).toThrow()
       expect(() => lits.run(`(regexp 'abc' :g 'extra')`)).toThrow()
     })
+
+    test(`email regexp`, () => {
+      expect(
+        lits.run(
+          `
+          (defn email? [string] (boolean (match (regexp '^(?:[a-z0-9!#$%&\\'*+/=?^_\`{|}~-]+(?:\\.[a-z0-9!#$%&\\'*+/=?^_\`{|}~-]+)*|\\'(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\\')@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])$') string)))
+          (email? 'albert.mojir@youcruit.com'),
+          `,
+        ),
+      ).toBe(true)
+    })
   })
 
   describe(`match`, () => {
