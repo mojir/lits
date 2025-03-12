2e031577a028dc867459ea6d8dfd489219213389
Albert Mojir <albert.mojir@youcruit.com>
2025-03-12 23:17:57 +0100

    Fixed playground search

--- playground-builder/src/allSearchResultEntries.ts
+++ playground-builder/src/allSearchResultEntries.ts
@@ -18,9 +18,11 @@ export const allSearchResultEntries: SearchResultEntry[] = searchables.map((refe
   const match = shortDescriptionRegExp.exec(reference.description)
   const description = match?.[1] ?? reference.description
 
+  const aliases = isFunctionReference(reference) ? reference.aliases?.join(' ') ?? '' : ''
+
   return {
     title: reference.title,
-    search: `${reference.title.replace(/&quot;/g, '"')} ${reference.category}`,
+    search: `${reference.title.replace(/&quot;/g, '"')} ${reference.category} ${aliases}`,
     html: getHtml(description, reference),
   }
 })
@@ -29,15 +31,22 @@ function getHtml(description: string, reference: Reference) {
   const title = escapeTitle(reference.title)
 
   return `
-    <div onclick="Playground.showPage('${reference.linkName}', 'smooth')" class="search-entry" ${styles('w-full', 'flex', 'flex-col', 'p-4', 'scroll-my-4', 'cursor-pointer', 'min-height: 10rem;')}>
+    <div onclick="Playground.showPage('${reference.linkName}', 'smooth')" class="search-entry" ${styles('w-full', 'flex', 'flex-col', 'p-4', 'scroll-my-4', 'cursor-pointer', 'min-height: 12rem;')}>
       <div ${styles('mb-4', 'flex', 'justify-between', 'items-baseline')}>
-        <div ${styles('text-lg', 'font-bold', 'text-color-gray-300')}>${title}</div>
+        <div ${styles('text-2xl', 'font-bold', 'text-color-gray-300')}>${title}</div>
         <div ${styles('text-base', 'text-color-gray-400')}>${reference.category}</div>
       </div>
       ${isFunctionReference(reference)
         ? `
           <div ${styles('text-base', 'mb-4')}>
             ${getFunctionSignature(reference)}
+                ${reference.aliases
+                  ? `<div ${styles('text-base', 'font-sans', 'mt-3', 'mb-1', 'text-color-white')}>${reference.aliases.length === 1 ? 'Alias' : 'Aliases'}</div>
+                    ${reference.aliases.map(alias =>
+                      getFunctionSignature({ ...reference, title: alias }),
+                    ).join('')}`
+                  : ''}
+
           </div>`
         : ''}
       <div ${styles('text-base')}>
