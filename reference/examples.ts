6e36217cd2e77d796b19ac2f38cfa5bd593b9fdd
Albert Mojir <albert.mojir@youcruit.com>
2025-03-19 16:45:03 +0100

    All examples converted

--- reference/examples.ts
+++ reference/examples.ts
@@ -414,54 +414,44 @@ end;
 start-game()    
     `.trim(),
   },
-  //   {
-  //     id: 'phone-number-formatter',
-  //     name: 'Phone number formatter',
-  //     description: 'Pretty prints a US phone number.',
-  //     code: `
-  // (defn formatPhoneNumber [$data]
-  //   (if (string? $data)
-  //     (do
-  //       (let [phoneNumber
-  //             (if (= "+" (nth $data 0))
-  //               (slice $data 2)
-  //               $data)])
-  //       (cond
-  //         (> (count phoneNumber) 6)
-  //           (str
-  //             "("
-  //             (slice phoneNumber 0 3)
-  //             ") "
-  //             (slice phoneNumber 3 6)
-  //             "-"
-  //             (slice phoneNumber 6))
+  {
+    id: 'phone-number-formatter',
+    name: 'Phone number formatter',
+    description: 'Pretty prints a US phone number.',
+    code: `
+function formatPhoneNumber(data)
+  if string?(data) then
+    let phoneNumber :=
+      if data[0] = "+" then
+        data slice 2
+      else
+        data
+      end;
 
-  //         (> (count phoneNumber) 3)
-  //           (str
-  //             "("
-  //             (slice phoneNumber 0 3)
-  //             ") "
-  //             (slice phoneNumber 3))
+    let length := count(phoneNumber);
 
-  //         (> (count phoneNumber) 0)
-  //           (str "(" (slice phoneNumber 0))
+    cond
+      case length > 6 then
+        "(" ++ slice(phoneNumber, 0, 3) ++ ") " ++ slice(phoneNumber, 3, 6) ++ "-" ++ slice(phoneNumber, 6)
+      case length > 3 then
+        "(" ++ slice(phoneNumber, 0, 3) ++ ") " ++ slice(phoneNumber, 3)
+      case length > 0 then
+        "(" ++ slice(phoneNumber, 0)
+    end ?? ""
+  else
+    ""
+  end
+end;
 
-  //         true
-  //           phoneNumber
-  //       )
-  //     )
-  //     ""
-  //   )
-  // )
 
-  // (write! formatPhoneNumber)
-  // (write! (formatPhoneNumber 123234))
-  // (write! (formatPhoneNumber "123234"))
-  // (write! (formatPhoneNumber "1232343456"))
-  // (write! (formatPhoneNumber "+11232343456789"))
-  // (write! (formatPhoneNumber "+11232343456"))
-  // `.trim(),
-  //   },
+write!(formatPhoneNumber);
+write!(formatPhoneNumber(123234));
+write!(formatPhoneNumber("123234"));
+write!(formatPhoneNumber("1232343456"));
+write!(formatPhoneNumber("+11232343456789"));
+write!(formatPhoneNumber("+11232343456"));
+  `.trim(),
+  },
   {
     id: 'factorial',
     name: 'Factorial',
@@ -495,138 +485,88 @@ end;
 sort(l, numberComparer)
       `.trim(),
   },
-  //   {
-  //     id: 'multiple-arity',
-  //     name: 'Many arities',
-  //     description: 'Function with multiple arities.',
-  //     code: `
-  // (defn foo
-  //   ([] "No parameters")
-  //   ([x] "One parameter")
-  //   ([x y] "Two parameters")
-  //   ([x y z] "Three parameters")
-  //   ([x y z zz &rest rest] "Four or more parameters")
-  // )
+  {
+    id: 'isoDateString',
+    name: 'Is ISO date string',
+    description: 'Check if string is formatted as an ISO date string.',
+    code: `
+function isoDateString?(data)
+  let m := data match #"^(\\d{4})-(\\d{2})-(\\d{2})$";
 
-  // (write! (foo))
-  // (write! (foo 1))
-  // (write! (foo 1 2))
-  // (write! (foo 1 2 3))
-  // (write! (foo 1 2 3 4))
-  // (write! (foo 1 2 3 4 5))
-  // `.trim(),
-  //   },
-  //   {
-  //     id: 'isoDateString',
-  //     name: 'Is ISO date string',
-  //     description: 'Check if string is formatted as an ISO date string.',
-  //     code: `
-  // (defn isoDateString? [$data]
-  //   (let [m
-  //            (match
-  //              $data
-  //              #"^(\\d{4})-(\\d{2})-(\\d{2})$")])
-  //   (if m
-  //     (do
-  //       (let
-  //         [
-  //           year (number (m 1))
-  //           month (number (m 2))
-  //           day (number (m 3))
-  //           leapYear
-  //             (&&
-  //               (zero? (mod year 4))
-  //               (||
-  //                 (! (zero? (mod year 100)))
-  //                 (zero? (mod year 400))
-  //               )
-  //             )
-  //         ])
-  //       (! (||
-  //         (|| (< year 1900) (> year 2100))
-  //         (|| (< month 1) (> month 12))
-  //         (|| (< day 1) (> day 31))
-  //         (&&
-  //           (||
-  //             (= month 4)
-  //             (= month 6)
-  //             (= month 9)
-  //             (= month 11))
-  //           (> day 30)
-  //         )
-  //         (&&
-  //           (= month 2)
-  //           (||
-  //             (&& leapYear (> day 29))
-  //             (&& (! leapYear) (> day 28))
-  //           )
-  //         )
-  //       ))
-  //     )
-  //     false
-  //   )
-  // )
+  if m then
+    let [year, month, day] := slice(m, 1) map number;
+    let leapYear := zero?(year mod 4) && (!zero?(year mod 100) || zero?(year mod 400));
 
-  // (write! (isoDateString? "1978-12-21"))
-  // (write! (isoDateString? "197-12-21"))
-  // `.trim(),
-  //   },
+    let invalid := 
+      (year < 1900 || year > 2100)
+      || (month < 1 || month > 12)
+      || (day < 1 || day > 31)
+      || day > 30 && (month = 4 || month = 6 || month = 9 || month = 11)
+      || month = 2 && (leapYear && day > 29 || !leapYear && day > 28);
 
-  //   {
-  //     id: 'label-from-value',
-  //     name: 'label-from-value',
-  //     description: 'Find label to corresponding value in array of {label value}-objects.',
-  //     code: `
-  // (defn label-from-value [$array $value]
-  //   (let [entry (some $array #(= $value (%1 :value)))])
-  //   (if (null? entry) (str $value) (entry :label))
-  // )
+    !(invalid)
+  else
+    false
+  end
+end;
 
-  // (def arr [
-  //   {:label "Name" :value "name"}
-  //   {:label "Age" :value "age"}
-  // ])
+write!(isoDateString?("1978-12-21"));
+write!(isoDateString?("197-12-21"));
+  `.trim(),
+  },
 
-  // (label-from-value arr :name)
-  // `.trim(),
-  //   },
+  {
+    id: 'label-from-value',
+    name: 'label-from-value',
+    description: 'Find label to corresponding value in array of { label, value }-objects.',
+    code: `
+function label-from-value(items, value)
+  let entry := items some (-> value = $["value"]);
+  if entry = null then
+    null
+  else
+    entry["label"]
+  end
+end;
 
-  //   {
-  //     id: 'labels-from-values',
-  //     name: 'labels-from-values',
-  //     description: 'Find labels to corresponding values in array of {label value}-objects.',
-  //     code: `
-  // (defn labels-from-values [$array $values]
-  //   (for
-  //     [
-  //       value
-  //       $values
-  //       &let [
-  //         label
-  //         (do
-  //           (let [entry
-  //                  (some
-  //                    $array
-  //                    #(= value (%1 :value)))])
-  //           (if
-  //             (null? entry)
-  //             (str value)
-  //             (entry :label))
-  //         )
-  //       ]
-  //       &when (boolean label)
-  //     ]
-  //     label
-  //   )
-  // )
 
-  // (def arr [
-  //   {:label "Name" :value "name"}
-  //   {:label "Age" :value "age"}
-  //   {:label "Email" :value "email"}
-  // ])
+let items := [
+  { label := "Name", value := "name" },
+  { label := "Age", value := "age" }
+];
 
-// (labels-from-values arr [:name :age])
-// `.trim(),
-//   },
+label-from-value(items, "name");
+  `.trim(),
+  },
+
+  {
+    id: 'labels-from-values',
+    name: 'labels-from-values',
+    description: 'Find labels to corresponding values in array of { label, value }-objects.',
+    code: `
+function labels-from-values($array, $values)
+  for
+    each value in $values,
+      let label := do
+        let entry := $array some -> value = $["value"];
+        if entry = null then
+          value
+        else
+          entry["label"]
+        end
+      end
+  do
+    label
+  end
+end;
+
+let arr := [
+  { label := "Name", value := "name" },
+  { label := "Age", value := "age" },
+  { label := "Email", value := "email" },
+];
+
+labels-from-values(arr, ["name", "age"])
+`.trim(),
+  },
 ]
